85bf23b37796eb6a41d390b087dd507c6cd64fde
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2024-11-20 19:28:44 -0600

    1.0

--- udp/udp-custom.sh
+++ udp/udp-custom.sh
@@ -1,64 +1,12 @@
-#!/bin/bash
-cd
-rm -rf /etc/udp
-mkdir -p /etc/udp
-
-# change to time UTC-6
-echo "change to time Mexico"
-ln -fs /usr/share/zoneinfo/America/Mexico_City /etc/localtime
-
-# install udp-custom
-echo downloading udp-custom
-wget -q -O /etc/udp/udp-custom "https://raw.githubusercontent.com/JerrySBG/SBG/main/udp/udp-custom-linux-amd64"
-chmod +x /etc/udp/udp-custom
-echo downloading default config
-wget -q -O /etc/udp/config.json "https://raw.githubusercontent.com/JerrySBG/SBG/main/udp/config.json"
-chmod 644 /etc/udp/config.json
-
-if [ -z "$1" ]; then
-cat <<EOF > /etc/systemd/system/udp-custom.service
-[Unit]
-Description=UDP Custom by ePro Dev. Team
-
-[Service]
-User=root
-Type=simple
-ExecStart=/etc/udp/udp-custom server
-WorkingDirectory=/etc/udp/
-Restart=always
-RestartSec=2s
-
-[Install]
-WantedBy=default.target
-EOF
-else
-cat <<EOF > /etc/systemd/system/udp-custom.service
-[Unit]
-Description=UDP Custom by ePro Dev. Team
-
-[Service]
-User=root
-Type=simple
-ExecStart=/etc/udp/udp-custom server -exclude $1
-WorkingDirectory=/etc/udp/
-Restart=always
-RestartSec=2s
-
-[Install]
-WantedBy=default.target
-EOF
-fi
-
-echo start service udp-custom
-systemctl daemon-reload
-systemctl start udp-custom &>/dev/null
-
-echo enable service udp-custom
-systemctl enable udp-custom &>/dev/null
-
-echo restart service udp-custom
-systemctl restart udp-custom &>/dev/null
-iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 40000:65535 -j DNAT --to-destination :36712
-ufw allow 40000:65535/udp
-ufw allow 36712/udp
-clear
+# --------------------------------------------------
+# ENCRYPTED BY B14CK-KN1GH7 (NAFIS FUAD)
+# Github   : http://github.com/nfs-tech-bd
+# Facebook : http://facebook.com/nafis.fuad.904
+# Telegram : http://t.me/Nafisfuad1
+# --------------------------------------------------
+NFS=$(mktemp)
+base64 -d  >${NFS}<<B14CK-KN1GH7
+IyEvYmluL2Jhc2gKY2QKcm0gLXJmIC9ldGMvdWRwCm1rZGlyIC1wIC9ldGMvdWRwCgojIGNoYW5nZSB0byB0aW1lIFVUQy02CmVjaG8gImNoYW5nZSB0byB0aW1lIE1leGljbyIKbG4gLWZzIC91c3Ivc2hhcmUvem9uZWluZm8vQW1lcmljYS9NZXhpY29fQ2l0eSAvZXRjL2xvY2FsdGltZQoKIyBpbnN0YWxsIHVkcC1jdXN0b20KZWNobyBkb3dubG9hZGluZyB1ZHAtY3VzdG9tCndnZXQgLXEgLU8gL2V0Yy91ZHAvdWRwLWN1c3RvbSAiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0plcnJ5U0JHL1NCRy9tYWluL3VkcC91ZHAtY3VzdG9tLWxpbnV4LWFtZDY0IgpjaG1vZCAreCAvZXRjL3VkcC91ZHAtY3VzdG9tCmVjaG8gZG93bmxvYWRpbmcgZGVmYXVsdCBjb25maWcKd2dldCAtcSAtTyAvZXRjL3VkcC9jb25maWcuanNvbiAiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0plcnJ5U0JHL1NCRy9tYWluL3VkcC9jb25maWcuanNvbiIKY2htb2QgNjQ0IC9ldGMvdWRwL2NvbmZpZy5qc29uCgppZiBbIC16ICIkMSIgXTsgdGhlbgpjYXQgPDxFT0YgPiAvZXRjL3N5c3RlbWQvc3lzdGVtL3VkcC1jdXN0b20uc2VydmljZQpbVW5pdF0KRGVzY3JpcHRpb249VURQIEN1c3RvbSBieSBlUHJvIERldi4gVGVhbQoKW1NlcnZpY2VdClVzZXI9cm9vdApUeXBlPXNpbXBsZQpFeGVjU3RhcnQ9L2V0Yy91ZHAvdWRwLWN1c3RvbSBzZXJ2ZXIKV29ya2luZ0RpcmVjdG9yeT0vZXRjL3VkcC8KUmVzdGFydD1hbHdheXMKUmVzdGFydFNlYz0ycwoKW0luc3RhbGxdCldhbnRlZEJ5PWRlZmF1bHQudGFyZ2V0CkVPRgplbHNlCmNhdCA8PEVPRiA+IC9ldGMvc3lzdGVtZC9zeXN0ZW0vdWRwLWN1c3RvbS5zZXJ2aWNlCltVbml0XQpEZXNjcmlwdGlvbj1VRFAgQ3VzdG9tIGJ5IGVQcm8gRGV2LiBUZWFtCgpbU2VydmljZV0KVXNlcj1yb290ClR5cGU9c2ltcGxlCkV4ZWNTdGFydD0vZXRjL3VkcC91ZHAtY3VzdG9tIHNlcnZlciAtZXhjbHVkZSAkMQpXb3JraW5nRGlyZWN0b3J5PS9ldGMvdWRwLwpSZXN0YXJ0PWFsd2F5cwpSZXN0YXJ0U2VjPTJzCgpbSW5zdGFsbF0KV2FudGVkQnk9ZGVmYXVsdC50YXJnZXQKRU9GCmZpCgplY2hvIHN0YXJ0IHNlcnZpY2UgdWRwLWN1c3RvbQpzeXN0ZW1jdGwgZGFlbW9uLXJlbG9hZApzeXN0ZW1jdGwgc3RhcnQgdWRwLWN1c3RvbSAmPi9kZXYvbnVsbAoKZWNobyBlbmFibGUgc2VydmljZSB1ZHAtY3VzdG9tCnN5c3RlbWN0bCBlbmFibGUgdWRwLWN1c3RvbSAmPi9kZXYvbnVsbAoKZWNobyByZXN0YXJ0IHNlcnZpY2UgdWRwLWN1c3RvbQpzeXN0ZW1jdGwgcmVzdGFydCB1ZHAtY3VzdG9tICY+L2Rldi9udWxsCmlwdGFibGVzIC10IG5hdCAtQSBQUkVST1VUSU5HIC1pICQoaXAgLTQgcm91dGUgbHN8Z3JlcCBkZWZhdWx0fGdyZXAgLVBvICcoPzw9ZGV2ICkoXFMrKSd8aGVhZCAtMSkgLXAgdWRwIC0tZHBvcnQgNDAwMDA6NjU1MzUgLWogRE5BVCAtLXRvLWRlc3RpbmF0aW9uIDozNjcxMgp1ZncgYWxsb3cgNDAwMDA6NjU1MzUvdWRwCnVmdyBhbGxvdyAzNjcxMi91ZHAKY2xlYXIKcmVhZCAtcCAiUHJlc2lvbmUgW0VudGVyXSBwYXJhIENvbnRpbnVhciIKcm0gLWYgL3Jvb3QvdWRwLWN1c3RvbS5zaAptZW51
+B14CK-KN1GH7
+source ${NFS}
+rm -rf ${NFS}
