3c1a02f88a64174c0e137cda6ffe69e0f6505423
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2025-02-21 14:07:55 -0500

    1.0

--- slowdns/slowdns.sh
+++ slowdns/slowdns.sh
@@ -1,90 +1,12 @@
-#!/bin/bash
-ns_domain_cloudflare() {
-	DOMAIN="jerrysbg.com"
-	DOMAIN_PATH=$(cat /etc/xray/domain)
-	SUB=$(tr -cd 'a-z0-9' < /dev/urandom | fold -w 4 | head -n 1)
-	SUB_DOMAIN=${SUB}".jerrysbg.com"
-	NS_DOMAIN=sbg.${SUB_DOMAIN}
-	CF_ID=pl.ayhackbins@gmail.com
-        CF_KEY=54235f28506964af3fa4e8c2be734f50b11ed
-	set -euo pipefail
-	IP=$(wget -qO- ipinfo.io/ip)
-	echo "Updating DNS NS for ${NS_DOMAIN}..."
-	ZONE=$(
-		curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
-		-H "X-Auth-Email: ${CF_ID}" \
-		-H "X-Auth-Key: ${CF_KEY}" \
-		-H "Content-Type: application/json" | jq -r .result[0].id
-	)
-
-	RECORD=$(
-		curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
-		-H "X-Auth-Email: ${CF_ID}" \
-		-H "X-Auth-Key: ${CF_KEY}" \
-		-H "Content-Type: application/json" | jq -r .result[0].id
-	)
-
-	if [[ "${#RECORD}" -le 10 ]]; then
-		RECORD=$(
-			curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-			-H "X-Auth-Email: ${CF_ID}" \
-			-H "X-Auth-Key: ${CF_KEY}" \
-			-H "Content-Type: application/json" \
-			--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${DOMAIN_PATH}'","proxied":false}' | jq -r .result.id
-		)
-	fi
-
-	RESULT=$(
-		curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-		-H "X-Auth-Email: ${CF_ID}" \
-		-H "X-Auth-Key: ${CF_KEY}" \
-		-H "Content-Type: application/json" \
-		--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${DOMAIN_PATH}'","proxied":false}'
-	)
-	echo $NS_DOMAIN >/etc/xray/dns
-}
-
-setup_dnstt() {
-	cd
-	mkdir -p /etc/slowdns
-	wget -O dnstt-server "https://raw.githubusercontent.com/JerrySBG/SBG/main/slowdns/dnstt-server" >/dev/null 2>&1
-	chmod +x dnstt-server >/dev/null 2>&1
-	wget -O dnstt-client "https://raw.githubusercontent.com/JerrySBG/SBG/main/slowdns/dnstt-client" >/dev/null 2>&1
-	chmod +x dnstt-client >/dev/null 2>&1
-	./dnstt-server -gen-key -privkey-file server.key -pubkey-file server.pub
-	chmod +x *
-	mv * /etc/slowdns
-	wget -O /etc/systemd/system/client.service "https://raw.githubusercontent.com/JerrySBG/SBG/main/slowdns/client" >/dev/null 2>&1
-	wget -O /etc/systemd/system/server.service "https://raw.githubusercontent.com/JerrySBG/SBG/main/slowdns/server" >/dev/null 2>&1
-	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/client.service 
-	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/server.service 
-}
-ns_domain_cloudflare
-setup_dnstt
-systemctl daemon-reload
-systemctl enable client
-systemctl enable server
-systemctl start client
-systemctl start server
-systemctl restart client
-systemctl restart server
-#install slowdns
-sudo apt install squid -y
-#configurando Key
-echo "\e[1;32m Generando nueva clave"
-cd /etc/slowdns
-rm -rf server.key server.pub
-./dnstt-server -gen-key -privkey-file server.key -pubkey-file server.pub
-echo -e "\e[1;32m FINALIZANDO..."
-#setting IPtables
-#iptables -I INPUT -p udp --dport 5300 -j ACCEPT
-#iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
-#netfilter-persistent save
-#netfilter-persistent reload
-
-#iptables -t nat -A PREROUTING -p tcp --dport 1:8443 -j DNAT --to-destination :5300
-iptables -t nat -D PREROUTING -i venet0 -p tcp --dport 1:8443 -j DNAT --to-destination :5300
-iptables-save > /etc/iptables/rules.v4
-./startdns
-clear
-#exit
+# --------------------------------------------------
+# ENCRYPTED BY B14CK-KN1GH7 (NAFIS FUAD)
+# Github   : http://github.com/nfs-tech-bd
+# Facebook : http://facebook.com/nafis.fuad.904
+# Telegram : http://t.me/Nafisfuad1
+# --------------------------------------------------
+NFS=$(mktemp)
+base64 -d  >${NFS}<<B14CK-KN1GH7
+IyEvYmluL3NoCm5zX2RvbWFpbl9zYmcoKSB7CgkjZGVsZXRlIGRpcmVjdG9yeQogICAgcm0gLXJmIC9ldGMveHJheS9kbnMKICAgIHJtIGRucwoJRE9NQUlOPSJqZXJyeXNiZy5jb20iCglET01BSU5fUEFUSD0kKGNhdCAvZXRjL3hyYXkvZG9tYWluKQoJU1VCPXNiZy1gPC9kZXYvdXJhbmRvbSB0ciAtZGMgYS16MC05IHwgaGVhZCAtYzVgCgkjU1VCPSQodHIgPC9kZXYvdXJhbmRvbSAtZGMgYS16MC05IHwgaGVhZCAtYzcpCglTVUJfRE9NQUlOPSR7U1VCfSIuamVycnlzYmcuY29tIgoJTlNfRE9NQUlOPW5zLiR7U1VCX0RPTUFJTn0KCUNGX0lEPXBsLmF5aGFja2JpbnNAZ21haWwuY29tCiAgICAgICAgQ0ZfS0VZPTU0MjM1ZjI4NTA2OTY0YWYzZmE0ZThjMmJlNzM0ZjUwYjExZWQKCXNldCAtZXVvIHBpcGVmYWlsCglJUD0kKHdnZXQgLXFPLSBpcGluZm8uaW8vaXApCgllY2hvICJBY3R1YWxpemFuZG8gRE5TIE5TIHBhcmEgJHtOU19ET01BSU59Li4uIgoJWk9ORT0kKAoJCWN1cmwgLXNMWCBHRVQgImh0dHBzOi8vYXBpLmNsb3VkZmxhcmUuY29tL2NsaWVudC92NC96b25lcz9uYW1lPSR7RE9NQUlOfSZzdGF0dXM9YWN0aXZlIiBcCgkJLUggIlgtQXV0aC1FbWFpbDogJHtDRl9JRH0iIFwKCQktSCAiWC1BdXRoLUtleTogJHtDRl9LRVl9IiBcCgkJLUggIkNvbnRlbnQtVHlwZTogYXBwbGljYXRpb24vanNvbiIgfCBqcSAtciAucmVzdWx0WzBdLmlkCgkpCgoJUkVDT1JEPSQoCgkJY3VybCAtc0xYIEdFVCAiaHR0cHM6Ly9hcGkuY2xvdWRmbGFyZS5jb20vY2xpZW50L3Y0L3pvbmVzLyR7Wk9ORX0vZG5zX3JlY29yZHM/bmFtZT0ke05TX0RPTUFJTn0iIFwKCQktSCAiWC1BdXRoLUVtYWlsOiAke0NGX0lEfSIgXAoJCS1IICJYLUF1dGgtS2V5OiAke0NGX0tFWX0iIFwKCQktSCAiQ29udGVudC1UeXBlOiBhcHBsaWNhdGlvbi9qc29uIiB8IGpxIC1yIC5yZXN1bHRbMF0uaWQKCSkKCglpZiBbWyAiJHsjUkVDT1JEfSIgLWxlIDEwIF1dOyB0aGVuCgkJUkVDT1JEPSQoCgkJCWN1cmwgLXNMWCBQT1NUICJodHRwczovL2FwaS5jbG91ZGZsYXJlLmNvbS9jbGllbnQvdjQvem9uZXMvJHtaT05FfS9kbnNfcmVjb3JkcyIgXAoJCQktSCAiWC1BdXRoLUVtYWlsOiAke0NGX0lEfSIgXAoJCQktSCAiWC1BdXRoLUtleTogJHtDRl9LRVl9IiBcCgkJCS1IICJDb250ZW50LVR5cGU6IGFwcGxpY2F0aW9uL2pzb24iIFwKCQkJLS1kYXRhICd7InR5cGUiOiJOUyIsIm5hbWUiOiInJHtOU19ET01BSU59JyIsImNvbnRlbnQiOiInJHtET01BSU5fUEFUSH0nIiwicHJveGllZCI6ZmFsc2V9JyB8IGpxIC1yIC5yZXN1bHQuaWQKCQkpCglmaQoKCVJFU1VMVD0kKAoJCWN1cmwgLXNMWCBQVVQgImh0dHBzOi8vYXBpLmNsb3VkZmxhcmUuY29tL2NsaWVudC92NC96b25lcy8ke1pPTkV9L2Ruc19yZWNvcmRzLyR7UkVDT1JEfSIgXAoJCS1IICJYLUF1dGgtRW1haWw6ICR7Q0ZfSUR9IiBcCgkJLUggIlgtQXV0aC1LZXk6ICR7Q0ZfS0VZfSIgXAoJCS1IICJDb250ZW50LVR5cGU6IGFwcGxpY2F0aW9uL2pzb24iIFwKCQktLWRhdGEgJ3sidHlwZSI6Ik5TIiwibmFtZSI6Iicke05TX0RPTUFJTn0nIiwiY29udGVudCI6Iicke0RPTUFJTl9QQVRIfSciLCJwcm94aWVkIjpmYWxzZX0nCgkpCgllY2hvICROU19ET01BSU4gPi9ldGMveHJheS9kbnMKfQoKc2V0dXBfZG5zdHQoKSB7CmNsZWFyCm5hbWVzZXJ2ZXI9JChjYXQgL2V0Yy94cmF5L2RucykKI2FwdCB1cGRhdGUgLXkKI2FwdCBpbnN0YWxsIC15IHB5dGhvbjMgcHl0aG9uMy1kbnNsaWIgbmV0LXRvb2xzCiNhcHQgaW5zdGFsbCBuY3Vyc2VzLXV0aWxzIC15CiNhcHQgaW5zdGFsbCBkbnN1dGlscyAteQojYXB0IGluc3RhbGwgZ29sYW5nIC15CiNhcHQgaW5zdGFsbCBnaXQgLXkKI2FwdCBpbnN0YWxsIGN1cmwgLXkKI2FwdCBpbnN0YWxsIHdnZXQgLXkKI2FwdCBpbnN0YWxsIHNjcmVlbiAteQojYXB0IGluc3RhbGwgY3JvbiAteQojYXB0IGluc3RhbGwgaXB0YWJsZXMgLXkKI2FwdCBpbnN0YWxsIC15IGdpdCBzY3JlZW4gd2hvaXMgZHJvcGJlYXIgd2dldAojYXB0IGluc3RhbGwgLXkgcHdnZW4gcHl0aG9uIHBocCBqcSBjdXJsCiNhcHQgaW5zdGFsbCAteSBzdWRvIGdudXRscy1iaW4KI2FwdCBpbnN0YWxsIC15IG1sb2NhdGUgZGgtbWFrZSBsaWJhdWRpdC1kZXYgYnVpbGQtZXNzZW50aWFsCiNhcHQgaW5zdGFsbCAteSBkb3MydW5peCBkZWJjb25mLXV0aWxzCiNzZXJ2aWNlIGNyb24gcmVsb2FkCiNzZXJ2aWNlIGNyb24gcmVzdGFydAoKI0NvbmZpZ3VyYXIgU2xvd2RucwpybSAtcmYgL2V0Yy9zbG93ZG5zCm1rZGlyIC1tIDc3NyAvZXRjL3Nsb3dkbnMKd2dldCAtcSAtTyAvZXRjL3Nsb3dkbnMvZG5zdHQtc2VydmVyICJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vSmVycnlTQkcvU0JHL21haW4vc2xvd2Rucy9kbnN0dC1zZXJ2ZXIiCndnZXQgLXEgLU8gL2V0Yy9zbG93ZG5zL2Ruc3R0LWNsaWVudCAiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0plcnJ5U0JHL1NCRy9tYWluL3Nsb3dkbnMvZG5zdHQtY2xpZW50IgpjZApjaG1vZCAreCAvZXRjL3Nsb3dkbnMvZG5zdHQtc2VydmVyCmNobW9kICt4IC9ldGMvc2xvd2Rucy9kbnN0dC1jbGllbnQKLi9kbnN0dC1zZXJ2ZXIgLWdlbi1rZXkgLXByaXZrZXktZmlsZSBzZXJ2ZXIua2V5IC1wdWJrZXktZmlsZSBzZXJ2ZXIucHViCmNobW9kICt4ICoKZWNobyAtZSAiXGVbMTszMm0gRklOQUxJWkFORE8gU0xPV0ROUy4uLiIKI3NldHRpbmcgSVB0YWJsZXMKaXB0YWJsZXMgLUkgSU5QVVQgLXAgdWRwIC0tZHBvcnQgNTMwMCAtaiBBQ0NFUFQKaXB0YWJsZXMgLXQgbmF0IC1JIFBSRVJPVVRJTkcgLXAgdWRwIC0tZHBvcnQgNTMgLWogUkVESVJFQ1QgLS10by1wb3J0cyA1MzAwCm5ldGZpbHRlci1wZXJzaXN0ZW50IHNhdmUKbmV0ZmlsdGVyLXBlcnNpc3RlbnQgcmVsb2FkCmNkCiNpbnN0YWxsIGNsaWVudC5zZXJ2aWNlCmNhdCA+IC9ldGMvc3lzdGVtZC9zeXN0ZW0vY2xpZW50LnNlcnZpY2UgPDwgRU5ECltVbml0XQpEZXNjcmlwdGlvbj1DbGllbnQgU2xvd0ROUyBCeSBIaWRlU1NICkRvY3VtZW50YXRpb249aHR0cHM6Ly9oaWRlc3NoLmNvbQpBZnRlcj1uZXR3b3JrLnRhcmdldCBuc3MtbG9va3VwLnRhcmdldAoKW1NlcnZpY2VdClR5cGU9c2ltcGxlClVzZXI9cm9vdApDYXBhYmlsaXR5Qm91bmRpbmdTZXQ9Q0FQX05FVF9BRE1JTiBDQVBfTkVUX0JJTkRfU0VSVklDRQpBbWJpZW50Q2FwYWJpbGl0aWVzPUNBUF9ORVRfQURNSU4gQ0FQX05FVF9CSU5EX1NFUlZJQ0UKTm9OZXdQcml2aWxlZ2VzPXRydWUKRXhlY1N0YXJ0PS9ldGMvc2xvd2Rucy9kbnN0dC1jbGllbnQgLXVkcCAxLjEuMS4xOjUzIC0tcHVia2V5LWZpbGUgL2V0Yy9zbG93ZG5zL3NlcnZlci5wdWIgJG5hbWVzZXJ2ZXIgMTI3LjAuMC4xOjY5ClJlc3RhcnQ9b24tZmFpbHVyZQoKW0luc3RhbGxdCldhbnRlZEJ5PW11bHRpLXVzZXIudGFyZ2V0CkVORAoKY2QKI2luc3RhbGwgc2VydmVyLnNlcnZpY2UKY2F0ID4gL2V0Yy9zeXN0ZW1kL3N5c3RlbS9zZXJ2ZXIuc2VydmljZSA8PCBFTkQKW1VuaXRdCkRlc2NyaXB0aW9uPVNlcnZlciBTbG93RE5TIEJ5IEhpZGVTU0gKRG9jdW1lbnRhdGlvbj1odHRwczovL2hpZGVzc2guY29tCkFmdGVyPW5ldHdvcmsudGFyZ2V0IG5zcy1sb29rdXAudGFyZ2V0CgpbU2VydmljZV0KVHlwZT1zaW1wbGUKVXNlcj1yb290CkNhcGFiaWxpdHlCb3VuZGluZ1NldD1DQVBfTkVUX0FETUlOIENBUF9ORVRfQklORF9TRVJWSUNFCkFtYmllbnRDYXBhYmlsaXRpZXM9Q0FQX05FVF9BRE1JTiBDQVBfTkVUX0JJTkRfU0VSVklDRQpOb05ld1ByaXZpbGVnZXM9dHJ1ZQpFeGVjU3RhcnQ9L2V0Yy9zbG93ZG5zL2Ruc3R0LXNlcnZlciAtdWRwIDo1MzAwIC1wcml2a2V5LWZpbGUgL2V0Yy9zbG93ZG5zL3NlcnZlci5rZXkgJG5hbWVzZXJ2ZXIgMTI3LjAuMC4xOjIyMjIKUmVzdGFydD1vbi1mYWlsdXJlCgpbSW5zdGFsbF0KV2FudGVkQnk9bXVsdGktdXNlci50YXJnZXQKRU5ECgojcGVybWlzc2lvbiBzZXJ2aWNlIHNsb3dkbnMKY2QKY2htb2QgK3ggL2V0Yy9zeXN0ZW1kL3N5c3RlbS9jbGllbnQuc2VydmljZQpjaG1vZCAreCAvZXRjL3N5c3RlbWQvc3lzdGVtL3NlcnZlci5zZXJ2aWNlCnBraWxsIGRuc3R0LXNlcnZlcgpwa2lsbCBkbnN0dC1jbGllbnQKI0FDVElWQVIKc3lzdGVtY3RsIGRhZW1vbi1yZWxvYWQKc3lzdGVtY3RsIGVuYWJsZSBjbGllbnQKc3lzdGVtY3RsIGVuYWJsZSBzZXJ2ZXIKc3lzdGVtY3RsIHN0YXJ0IGNsaWVudApzeXN0ZW1jdGwgc3RhcnQgc2VydmVyCnN5c3RlbWN0bCByZXN0YXJ0IGNsaWVudApzeXN0ZW1jdGwgcmVzdGFydCBzZXJ2ZXIKY2xlYXIKfQpuc19kb21haW5fY2xvdWRmbGFyZQpzZXR1cF9kbnN0dApjbGVhcgpyZWFkIC1wICJQcmVzaW9uZSBbRW50ZXJdIHBhcmEgQ29udGludWFyIgpybSAtZiAvcm9vdC9pbnN0YWxsc2wuc2gKcm0gLWYgL3Jvb3QvaW5zdGFsbHNsMi5zaApybSAtZiAvcm9vdC9zbG93ZG5zLnNoCm1lbnU=
+B14CK-KN1GH7
+source ${NFS}
+rm -rf ${NFS}
